70f481f87f0357775be146e99a805f4ac3a5807c
ztzz <ztzz.xyz@gmail.com>
2025-04-17 10:39:08 +0800

    添加星舰机制

--- Tests.cs
+++ Tests.cs
@@ -12,9 +12,9 @@ namespace Hearthbuddy_extension_test
         {
             AiTest test = new AiTest();
             test.MainPath = @"D:\hb\r\Hearthbuddy\Routines\DefaultRoutine\Silverfish";
-            test.TestFilePath = @"Test\Data\对局记录\日期2025-04-05\16-16-14-MAGE-PRIEST\3法力水晶 第6回合 第4步操作.txt";
+            test.TestFilePath = @"Test\Data\对局记录\日期2025-04-05\18-04-37-SHAMAN-PRIEST\7法力水晶 第7回合 第0步操作.txt";
             test.CardDbPath = "data";
-            test.Bot = new Behavior丨狂野丨奥秘法();
+            test.Bot = new Behavior丨通用丨不设惩罚();
             test.Test();
         }
     }
