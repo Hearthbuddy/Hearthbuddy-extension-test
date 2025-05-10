560f2d9ebbe28708a16e44c65d98d5714549b410
ztzz <ztzz.xyz@gmail.com>
2025-05-10 12:47:30 +0800

    test框架修改：保证先进行carddefs.xml路径设置，再进行carddb加载

--- Tests.cs
+++ Tests.cs
@@ -11,10 +11,10 @@ namespace Hearthbuddy_extension_test
         public void Test1()
         {
             AiTest test = new AiTest();
-            test.MainPath = @"D:\hb\r\Hearthbuddy\Routines\DefaultRoutine\Silverfish";
-            test.TestFilePath = @"Test\Data\对局记录\日期2025-04-05\18-04-37-SHAMAN-PRIEST\7法力水晶 第7回合 第0步操作.txt";
+            test.MainPath = @"D:\hb\stand\Hearthbuddy\Routines\DefaultRoutine\Silverfish";
+            test.TestFilePath = @"Test\Data\对局记录\日期2025-05-05\10-25-39-PRIEST-DRUID\1法力水晶 第2回合 第0步操作.txt";
             test.CardDbPath = "data";
-            test.Bot = new Behavior丨通用丨不设惩罚();
+            test.Bot = new Behavior丨狂野丨快攻暗牧();
             test.Test();
         }
     }
